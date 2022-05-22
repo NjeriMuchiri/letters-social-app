31c460342dff17834e39846e8636a3e90a8657cf
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-05-22 12:31:48 +0300

    Creating the app page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import './App.css';
+import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
+import parseLinkHeader from 'parse-link-header';
+import orderBy  from 'lodash/orderBy';
 
-function App() {
-  return (
-    <div className="App">
+import ErrorMessage from './components/error/Error';
+import Loader from './components/Loader';
+import * as API from './shared/http';
+import Ad from './components/ad/Ad';
+import Navbar from './components/nav/navbar';
+import Welcome from './components/welcome/Welcome'
+
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      error: null,
+      loading: false,
+      posts: [],
+      endpoint: `${process.env.ENDPOINT}/posts?_page=1&_sort=date&_order=DESC&_embed=comments&expand=user&_embed=likes`
+    };
+  }
+  static propTypes = {
+    children: PropTypes.node
+  };
+  render(){
+    return(
+      <div className='app'>
+       <Navbar />
+       {this.state.loading ? (
+         <div className='loading'>
+        <Loader />
+        </div>
+       ) : (
+         <div className='home'> 
+         <Welcome />
+         <div>
+           <button className='block'>
+             Load more
+           </button>
+         </div>
+         <div>
+           <Ad 
+           url ="https://ifelse.io/book"
+           imageUrl="/static/assets/ads/ria.png"
+           />
+           <Ad 
+            url = "https://ifelse.io/book"
+            imageUrl="/static/assets/ads/orly.jpg"
+           />
+           </div>
+         </div>
+       )}
+      </div>
+    );
+  }
       
-    </div>
-  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import {render} from 'react-dom';
+import React from 'react'; 
 
 import App from './App';
 
 import './shared/crash';
 import './shared/service-worker';
 import './shared/vendor';
-// import '.styles/styles.scss';
+import '.styles/styles.scss';
 
 render(<App />, document.getElementById('app'));
 
